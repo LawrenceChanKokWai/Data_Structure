20793baf289dcc3332c8ae125ee93ce2057a2122
Chan Kok Wai <chankokwai@Chans-MacBook-Air.local>
2023-07-12 14:10:37 +0800

    added linear search

---
+++ Headers/LinearSearch.h
@@ -0,0 +1,10 @@
+
+#ifndef DATASTRUCTURE_LINEARSEARCH_H
+#define DATASTRUCTURE_LINEARSEARCH_H
+
+class LinearSearch {
+public:
+    bool linearSearch(int myArray[], int length, int& value);
+};
+
+#endif //DATASTRUCTURE_LINEARSEARCH_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "./Headers/LinearSearch.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int numArray[] = {10, 50, 30, 28, 70, 183, 67, 35};
+    int length = sizeof(numArray) / sizeof(numArray[0]);
+    int value = 183;
+
+    LinearSearch mySearch;
+
+    std::cout << mySearch.linearSearch(numArray, length, value);
+
     return 0;
 }
